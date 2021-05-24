17e60184c63ec78ac1f79658181070268bd755e9
Itzkartik <smartzboi@gmail.com>
2021-05-24 14:04:19 +0530

    work

--- admin.php
+++ admin.php
@@ -1,5 +1,6 @@
 <?php
 // Make something md5
+
 // $password = "38ba0ef529faec6dc4f8bcdba153c9e0";
 // $username = "Hari";
 // if ($_SERVER["REQUEST_METHOD"] == "POST") {
@@ -14,12 +15,40 @@
 //   }
 // }
 
-$inp = file_get_contents('json/main.json');
-$tempArray = json_decode($inp, true);
+$inp;
+$tempArray;
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  $inp = file_get_contents('json/main.json');
+  $tempArray = json_decode($inp, true);
+  if ($_GET['change'] == "texture") {
+    upload_img($_FILES["img_file"], $_GET['texture_type']);
+  } else if ($_GET['change'] == "color") {
+    fill_color(array("name" => $_POST['name'], "hex_code" => $_POST['hex_code']));
+  } else if ($_GET['change'] == "price") {
+    fill_price(array($_POST['price']));
+  } else {
+    echo "Failed";
+  }
+}
 
-// fill_textures(array("img_name"=>"5.jpeg"), "exterior");
-// fill_color(array("name"=>"White", "hex_code"=>"0xffffff"));
-// fill_price(array("name"=>"White", "hex_code"=>"0xffffff"));
+function upload_img($img_file, $tex_type){
+  $target_dir = "tex/";
+  $target_file = $target_dir . basename($img_file["name"]);
+  $check = getimagesize($img_file["tmp_name"]);
+  if($check !== false) {
+    if (file_exists($target_file)) {
+      echo "Sorry, file already exists.";
+    } else {
+      if (move_uploaded_file($img_file["tmp_name"], $target_file)) {
+        fill_textures(array("img_name" => $img_file["name"]), $tex_type);
+      } else {
+        echo "Sorry, there was an error uploading your file.";
+      }
+    }
+  } else {
+    echo "File is not an image.";
+  }
+}
 
 function fill_color($data)
 {
@@ -32,9 +61,9 @@ function fill_color($data)
 function fill_textures($data, $tex_type)
 {
   global $tempArray;
-  if($tex_type == 'interior'){
+  if ($tex_type == 'interior') {
     array_push($tempArray["interior"], $data);
-  }else{
+  } else {
     array_push($tempArray["exterior"], $data);
   }
   $jsonData = json_encode($tempArray);
@@ -48,3 +77,5 @@ function fill_price($data)
   $jsonData = json_encode($tempArray);
   file_put_contents('json/main.json', $jsonData);
 }
+
+?>
