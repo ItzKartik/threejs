4665f180fcaea808e24e46816ae27e650bf5e898
ItzKartik <smartzboi@gmail.com>
2021-06-23 21:58:06 +0530

    work

--- js/index.js
+++ js/index.js
@@ -1,18 +1,21 @@
 import { OrbitControls } from "https://threejs.org/examples/jsm/controls/OrbitControls.js";
 
+default_objs = ['MF_1.glb', 'MH_1.glb', 'MH_1 (Exterior Skin).glb', 'MH_2.glb', 'MH_2 (Exterior Skin).glb', 'MW_1.glb', 'MW_1 (Exterior Skin).glb', 'MW_2.glb', 'MW_2 (Exterior Skin).glb']
+S110F_objs = ['MF_1.glb', 'MR_1Copy1.glb', 'MW_1_exteriorskin.glb', 'MW_1.glb', 'MW_2_exteriorskin.glb', 'MW_2.glb', 'MW_3_exteriorskin.glb', 'MW_3.glb', 'MW_4_exteriorskin.glb', 'MW_4.glb']
+
 function init() {
     scene = new THREE.Scene();
     scene.background = new THREE.Color(0xffffff);
 
-    camera = new THREE.PerspectiveCamera(80, window.innerWidth / window.innerHeight, 1, 800);
+    camera = new THREE.PerspectiveCamera(40, window.innerWidth / window.innerHeight, 10, 1800);
     camera.position.z = 100;
-    camera.position.y = 10;
-    camera.position.x = -80;
+    camera.position.y = 20;
+    camera.position.x = -180;
 
-    renderer = new THREE.WebGLRenderer({antialias: true });
+    renderer = new THREE.WebGLRenderer({ antialias: true });
     renderer.setSize(window.innerWidth, window.innerHeight);
     renderer.shadowMap.enabled = true;
-    renderer.setPixelRatio(window.devicePixelRatio); 
+    renderer.setPixelRatio(window.devicePixelRatio);
 
     document.body.appendChild(renderer.domElement);
 
@@ -21,47 +24,38 @@ function init() {
     controls.rotateSpeed = 0.5;
     controls.zoomSpeed = 0.9;
 
-    controls.minDistance = 20;
-    controls.maxDistance = 30;
-
     controls.minPolarAngle = 0;
     controls.maxPolarAngle = Math.PI / 2;
 
     controls.enableDamping = true;
     controls.dampingFactor = 0.05;
 
-    light = new THREE.PointLight(0xffffcc, 20, 200);
-    light.position.set(0, 20, -10);
+    var hemiLight = new THREE.HemisphereLight(0xffeeb1, 0x080820, 2);
+    scene.add(hemiLight);
+    light = new THREE.SpotLight(0xffa95c, 0.2);
+    light.position.set(-50,50,50);
     light.castShadow = true;
-    scene.add(light);
+    scene.add( light );
 
-    // var hemiLight = new THREE.HemisphereLight(0xffffff, 0xffffff, 0.61);
-    // hemiLight.position.set(0, 50, 0);
-    // // Add hemisphere light to scene   
-    // scene.add(hemiLight);
+    // light = new THREE.PointLight(0xffffcc, 10, 200);
+    // light.position.set(0, 150, 0);
+    // light.castShadow = true;
+    // scene.add(light);
 
-    // var dirLight = new THREE.DirectionalLight(0xffffff, 0.54);
-    // dirLight.position.set(10, 10, 10);
-    // dirLight.castShadow = true;
-    // dirLight.shadow.mapSize = new THREE.Vector2(1024, 1024)
-    // scene.add(dirLight);
+    // var light2 = new THREE.PointLight(0xffffcc, 10, 200);
+    // light2.position.set(0, -200, 0);
+    // light2.castShadow = true;
+    // scene.add(light2);
 
-    // var floorGeometry = new THREE.PlaneGeometry(5000, 5000, 1, 1);
-    // var floorMaterial = new THREE.MeshPhongMaterial({
-    // color: 0xffffff,
-    // shininess: 0
-    // });
-
-    // var floor = new THREE.Mesh(floorGeometry, floorMaterial);
-    // floor.rotation.x = -0.5 * Math.PI;
-    // floor.receiveShadow = true;
-    // floor.position.y = -1;
-    // scene.add(floor);
+    // var light3 = new THREE.PointLight(0xffffcc, 10, 200);
+    // light3.position.set(0, 0, 0);
+    // light3.castShadow = true;
+    // scene.add(light3);
 
     loader = new THREE.GLTFLoader();
     loader.crossOrigin = true;
 
-    load_obj('9.jpeg');
+    load_obj(default_objs, 'objs/default/', '5.jpeg');
 
     render();
 
@@ -86,25 +80,25 @@ function resizeRendererToDisplaySize(renderer) {
     return needResize;
 }
 
-export function load_obj(imgfile) {
-    textureLoader = new THREE.TextureLoader();
-    textureLoader.encoding = THREE.sRGBEncoding;
-    texture = textureLoader.load("tex/"+imgfile);
-    texture.flipY = false;
-
-    loader.load('foo.glb', function (data) {
-        object = data.scene;
-        object.scale.set(2, 2, 2);
-        // object.rotation.y = Math.PI;
-        object.traverse((o) => {
-            if (o.isMesh) {
-                o.material.map = texture;
-                o.material.map.needsUpdate = true;
-            }
+export function load_obj(obj, type, imgfile) {
+    for(var i=0; i < obj.length; i++){
+        textureLoader = new THREE.TextureLoader();
+        // textureLoader.encoding = THREE.sRGBEncoding;
+        texture = textureLoader.load("tex/" + imgfile);
+        loader.load(type+obj[i], function (data) {
+            object = data.scene;
+            object.scale.set(2, 2, 2);
+            object.rotation.x = Math.PI / 2;
+            object.rotation.y = Math.PI;
+            object.rotation.z = Math.PI / 2;
+            object.traverse((o) => {
+                if (o.isMesh) {
+                    o.material.map = texture;
+                }
+            });
+            scene.add(object);
         });
-        scene.add(object);
-    });
-
+    }
 }
 
 export function change_color(hex) {
